ba82b8b8eb014fc4d0dba812e3c3121df68fdccd
OjModupe <134464057+OjModupe@users.noreply.github.com>
2023-10-07 13:52:41 +0000

    C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Determine if a random number is positive or negative or zero
+ *
+ * Description: This program generates a random number and
+ * then determines if it's positive, negative, or zero.
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+    int n; /* Variable declaration */
+
+    srand(time(0));
+    n = rand() - RAND_MAX / 2;
+
+    /* Determine and print the sign of n */
+    if (n < 0)
+    {
+        printf("%d is %s\n", n, "negative");
+    }
+    else if (n > 0)
+    {
+        printf("%d is %s\n", n, "positive");
+    }
+    else
+    {
+        printf("%d is %s\n", n, "zero");
+    }
+
+    return (0);
+}
