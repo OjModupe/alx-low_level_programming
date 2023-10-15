85fb7d695f889b8d1832ddb61e5c52b70a2a80fb
OjModupe <134464057+OjModupe@users.noreply.github.com>
2023-10-15 23:31:10 +0000

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,33 +3,27 @@
 #include <stdio.h>
 
 /**
- * main - Determine if a random number is positive or negative or zero
+ * main - Entry point
  *
- * Description: This program generates a random number and
- * then determines if it's positive, negative, or zero.
- *
- * Return: 0 on success
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-  int n; /* Variable declaration */
+	int n;
 
-  srand(time(0));
-    n = rand() - RAND_MAX / 2;
-
-    /* Determine and print the sign of n */
-    if (n < 0)
-    {
-        printf("%d is %s\n", n, "negative");
-    }
-    else if (n > 0)
-    {
-        printf("%d is %s\n", n, "positive");
-    }
-    else
-    {
-        printf("%d is %s\n", n, "zero");
-    }
-
-    return (0);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
+	else
+	{
+		printf("%d is zero\n", n);
+	}
+	return (0);
 }
