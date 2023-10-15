79cfffb9fc004e4c703e847bc12484930d7376c4
OjModupe <134464057+OjModupe@users.noreply.github.com>
2023-10-15 23:42:38 +0000

    3-print_alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,31 +1,25 @@
 #include <stdio.h>
-#include <ctype.h>
 
 /**
  * main - Entry point
- *
- * Description: Prints lowercase letters from 'a' to 'z' and uppercase letters
- * from 'A' to 'Z' followed by a new line.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    int lowerCase = 'a';
-    int upperCase = 'A';
+	int c;
+	int i;
 
-    while (lowerCase <= 'z')
-    {
-        putchar(lowerCase);
-        lowerCase += 1;
-    }
-
-    while (upperCase <= 'Z')
-    {
-        putchar(upperCase);
-        upperCase += 1;
-    }
-
-    putchar('\n');
-    return (0);
+	c = 'a';
+	for (i = 0; i < 26; i++)
+	{
+		putchar(c + i);
+	}
+	c = 'a';
+	for (i =  0; i < 26; i++)
+	{
+		putchar((c + i) - 32);
+	}
+	putchar(10);
+	return (0);
 }
