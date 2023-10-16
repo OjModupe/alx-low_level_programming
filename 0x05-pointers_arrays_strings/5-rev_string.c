007032b6d3a7ee5d891f8844ec94cdd1bbdf67f3
OjModupe <134464057+OjModupe@users.noreply.github.com>
2023-10-16 22:39:31 +0000

    5-rev_string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,11 +1,33 @@
 #include "main.h"
 
 /**
- * print_rev - function that prints a string, in reverse
+ * rev_string - a function that reverses a string.
  *
- * @s: char pointer variable
+ * @s: char pointer var
  *
- * Return: nothing.
+ * Return: nothing
  */
 
-void print_rev(char *s)
+void rev_string(char *s)
+{
+	int i, j;
+	char c;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	j = i - 1;
+	i = 0;
+	while (i < j)
+	{
+		c = s[j];
+		s[j] = s[i];
+		s[i] = c;
+
+		i++;
+		j--;
+	}
+}
